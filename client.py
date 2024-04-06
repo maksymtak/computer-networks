2630b0f63d87b7196aba6e7f97401c6a1b56ca84
maksymtak <156093888+maksymtak@users.noreply.github.com>
2024-04-06 19:29:53 +0200

    Add files via upload

--- client.py
+++ client.py
@@ -1,137 +1,225 @@
 
 import socket
+import threading
+
 
 SERVER_HOST = '212.132.114.68'
 SERVER_PORT = 5378
 host_port = (SERVER_HOST, SERVER_PORT)
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
 
 print('Welcome to Chat Client. Enter you login:')
 # Please put your code in this file
 
 
+def graceful_exit(sock):
+    print("goodbye")
+    sock.close()
+    exit()
+
+
 
 def bad_login_response(response, name):
-    try:
-        if 'BAD-RQST-BODY' in response:
-            print(f"Cannot log in as {name}. That username contains disallowed characters.")
-        elif 'BUSY' in response:
-            print("Cannot log in. The server is full!")
-    except OSError as msg:
-        print(msg)
-    
+    if 'BAD-RQST-BODY' in response:
+        print(f"Cannot log in as {name}. That username contains disallowed characters.")
+
+    elif 'BUSY' in response:
+        print("Cannot log in. The server is full!")
+    elif 'IN-USE' in response:
+        print(f'Cannot log in as {name}. That username is already in use.')
+    else:
+        print(response)
+
 
 
 
 def log_in():
-    
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect(host_port)
     logged = False
     while (not logged): # while username is not set
         username = input("") # get name 
+
+
+        if "!quit" in username:
+            graceful_exit(sock)
+
+
         string_bytes = (f"HELLO-FROM {username} \n").encode("utf-8") # encode it
-        print(string_bytes)
+        #print(string_bytes)
         bytes_len = len(string_bytes) 
         num_bytes_to_send = bytes_len
-        while num_bytes_to_send > 0: # send the name
-            num_bytes_to_send -= sock.send(string_bytes[bytes_len-num_bytes_to_send:])
+        try:
+            while num_bytes_to_send > 0: # send the name
+                num_bytes_to_send -= sock.send(string_bytes[bytes_len-num_bytes_to_send:])
+                
+            data = sock.recv(4096) # get login response
+        
 
-        data = sock.recv(4096) # get login response
-        if not data:
-            print("Socket is closed.")
+            if not data:
+                print("Socket is closed.")
+                graceful_exit(sock)
+                
+
+            else:
+                data = data.decode("utf-8")
+                # print(f"Read data from socket: {data}")
+                if 'HELLO' in data:
+                    x = data.split()
+                    print(f"Successfully logged in as {x[1]}!")
+                    return sock
+                    
+                else:
+                    sock.close() #buahahhahhaahah get reconnected
+                    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                    sock.connect(host_port)
+                    bad_login_response(data, username)
+            
+        except OSError as msg:
+            print(f"{msg}")
             
 
-        else:
-            data = data.decode("utf-8")
-            # print(f"Read data from socket: {data}")
-            if 'HELLO' in data:
-                x = data.split()
-                print(f"Successfully logged in as {x[1]}!")
-                return
-            else:
-                bad_login_response(data, username)
-
 def send_response(data):
+    
     if "SEND-OK" in data:
         print("The message was sent succesfully")
     elif "BAD-DEST-USER" in data:
         print("The destination user does not exist")
     elif "DELIVERY" in data: # this is a biiiiig assumption but the only other return I got was when messaging myself
         prt_message(data)
+    else:
+        print(data)
+        
 
-def send_message(terminal_input):
-    
+def send_message(terminal_input, sock):
     terminal_input = terminal_input.strip("@")
     x = terminal_input.split(" ", 1)
 
     string_bytes = (f"SEND {x[0]} {x[1]}\n").encode("utf-8") # encode it
+    # print(string_bytes)
     bytes_len = len(string_bytes) 
     num_bytes_to_send = bytes_len
     while num_bytes_to_send > 0: # send the name
         num_bytes_to_send -= sock.send(string_bytes[bytes_len-num_bytes_to_send:])
 
-    data = sock.recv(4096) # get login response
-    if not data:
-        print("Socket is closed.")
+    # data = sock.recv(4096) # get login response
+    # if not data:
+    #    print("Socket is closed.")
         
-    else:
-        data = data.decode("utf-8")
-        send_response(data)
+    #else:
+    #    data = data.decode("utf-8")
+    #    send_response(data)
 
 
     
 
-def get_active_list():
+def get_active_list(sock):
+
     string_bytes = ("LIST\n").encode("utf-8") # encode it
     bytes_len = len(string_bytes) 
     num_bytes_to_send = bytes_len
-    while num_bytes_to_send > 0: # send the request
-        num_bytes_to_send -= sock.send(string_bytes[bytes_len-num_bytes_to_send:])
+    try:
+        while num_bytes_to_send > 0: # send the request
+            num_bytes_to_send -= sock.send(string_bytes[bytes_len-num_bytes_to_send:])
+        # print("hih?")
+        # data = sock.recv(4096)
+        
+        # handle_response(data)
+        # print("hoh?")
+        # if not data:
+        #     print("Socket is closed.")
+        # else:
+        #    data = data.decode("utf-8")
+        #    plist = data.split(",")
+        #    plist[0] = plist[0].strip("LIST-OK ")
+        #    print(f"There are {len(plist)} online:")
 
-    data = sock.recv(4096) # get login response
-    if not data:
-        print("Socket is closed.")
-    else:
-        data = data.decode("utf-8")
-        plist = data.split(",")
-        plist[0] = plist[0].strip("LIST-OK ")
-        print(f"There are {len(plist)} online:")
-
-        for i in plist:
-            print(f"- {i} \n")
+    except OSError as msg:
+        print(f"{msg} losing it")
 
 
 def prt_message(message):
     x = message.split(" ", 2)
     print(f"From {x[1]}: {x[2]}")
 
-    
-
-def logged_in(): 
-    terminal_input = input("") # get name 
-    while not ("!quit" in terminal_input):
+def get_input(sock):
+    while True:
+        terminal_input = input("") # get name and mess
         if terminal_input.startswith("@"):
-            send_message(terminal_input)
+            send_message(terminal_input, sock)
         elif "!who" in terminal_input:
-            get_active_list()
-            return # it craches after :((
-            
+            get_active_list(sock)
+            # graceful_exit(sock)
+        elif "!quit" in terminal_input:
+            graceful_exit(sock)
         else:
             print("say what now?")
-            exit()
+            get_input(sock)
 
-        data = sock.recv(4096)
-        data = data.decode("utf-8")
-        if "DELIVERY" in data:
-            prt_message(data)
-        terminal_input = input("") # get input 
             
 
-try:
-    log_in()
-    logged_in()
-except OSError as msg:
-    print(msg)
+def print_list(data):
+    plist = data.split(",")
+    plist[0] = plist[0].strip("LIST-OK ")
+    print(f"There are {len(plist)} online:")
+    for i in plist:
+                print(f"- {i} \n")
+
+def handle_response(sock):
+    while True:
+        data = sock.recv(4096)
+        data = data.decode("utf-8")
+        if data:
+            if "DELIVERY" in data:
+                prt_message(data)
+            elif "LIST-OK" in data:
+                print_list(data)
+            elif "SEND-OK" in data:
+                print("The message was sent succesfully")
+            elif "BAD-DEST-USER" in data:
+                print("The destination user does not exist")
+            elif "BAD-RQST-HDR" in data:
+                print("Error: Unknown issue in previous message header.")
+            elif "BAD-RQST-BODY" in data:
+                print("Error: Unknown issue in previous message body.")
+            else:
+                print(data)
+        else:
+            print("Socket closed")
+        
+        
+
+
+
+def logged_in(sock): 
+
+    # terminal_input = input("") # get name and mess
+    #while not ("!quit" in terminal_input):
+    #    if terminal_input.startswith("@"):
+    #        send_message(terminal_input, sock)
+    #    elif "!who" in terminal_input:
+    #        get_active_list(sock)
+            # graceful_exit(sock)
+            # return # it craches after :((
+            
+    #    else:
+    #        print("say what now?")
+            # graceful_exit(sock)
+    #t = threading.Thread(target = get_input, args = (sock,))
+    #t.start()
+    #data = sock.recv(4096)
+    thr = threading.Thread(target = handle_response, args = (sock,), daemon=True)
+    thr.start()
+    get_input(sock)
+
+    
+
+s = log_in()
+logged_in(s)
+
+    
+
 
 
 
